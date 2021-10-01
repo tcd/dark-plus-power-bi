752ad8a4fa7c4e3322b4d182cfdbc4d5f47740b7
Clay Dunston <claydunston@gmail.com>
2021-10-01 16:31:29 -0500

    Clean up some code

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,16 @@
+// Node
 const fs     = require("fs").promises
 const path   = require("path")
-
+// NPM
 const pkgDir = require("pkg-dir")
 const JSON5  = require("json5")
 
-const buildSchema = async () => {
+/**
+ * @param {string} inPath 
+ * @param {string} outPath 
+ */
+const convertJson = async (inPath, outPath) => {
     try {
-        const rootDir = await pkgDir(__dirname)
-
-        const schemaInPath  = path.join(rootDir, "src", "json", "schema", "schema.jsonc")
-        const schemaOutPath = path.join(rootDir, "out", "report-theme.schema.json")
-
-        const oldSchemaFileContent = await fs.readFile(schemaInPath, "utf8")
-        const oldSchemaJSONContent = JSON5.parse(oldSchemaFileContent)
-        const newSchemaJSONContent = JSON.stringify(oldSchemaJSONContent, null, 4)
-
-        await fs.writeFile(schemaOutPath, newSchemaJSONContent)
-    
-        console.log(`file written: ${schemaOutPath}`)
-    } catch (error) {
-       console.log(error)
-       process.exit(1)
-    }
-}
-
-const buildTheme = async () => {
-    try {
-        const rootDir = await pkgDir(__dirname)
-
-        const inPath  = path.join(rootDir, "src", "json", "theme.jsonc")
-        const outPath = path.join(rootDir, "out", "dark-plus-report-theme.json")
-
         const oldFileContent = await fs.readFile(inPath, "utf8")
         const oldJSONContent = JSON5.parse(oldFileContent)
         const newJSONContent = JSON.stringify(oldJSONContent, null, 4)
@@ -44,17 +24,30 @@ const buildTheme = async () => {
     }
 }
 
-async function main() {
-    await buildTheme()
-    await buildSchema()
+const buildSchema = async () => {
+    const rootDir = await pkgDir(__dirname)
+    const inPath  = path.join(rootDir, "src", "json", "schema", "schema.json5")
+    const outPath = path.join(rootDir, "out", "report-theme.schema.json")
+    await convertJson(inPath, outPath)
 }
 
-(async () => {
+const buildTheme = async () => {
+    const rootDir = await pkgDir(__dirname)
+    const inPath  = path.join(rootDir, "src", "json", "theme.json5")
+    const outPath = path.join(rootDir, "out", "dark-plus-report-theme.json")
+    await convertJson(inPath, outPath)
+}
+
+async function main() {
     try {
-        await main()
-        process.exit(0)
+        await buildTheme()
+        await buildSchema()
     } catch (error) {
         console.log(error)    
         process.exit(1)
     }
+}
+
+(async () => {
+    await main()
 })()
